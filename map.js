4cb63e42ff7603b878dc8b0acebcd75b09a2c8f4
steveforrest <steveforrest_1@hotmail.com>
2021-09-16 13:26:04 +0000

    initial commit

--- map.js
+++ map.js
@@ -6,14 +6,32 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for(let num of nums){
+  results.push(num*2);
+}
+
+console.log(results)
 
 // Using map()
+
+const multiplyByTwo = function (num){
+  return num * 2;
+}
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
 
+const mapResults2 = nums.map(function(num) {return num * 2});
+console.log(mapResults2);
 
 // Simplfied w/ map() + arrow function
+
+const mapResults3 = nums.map(num => num * 2);
+console.log(mapResults3);
 
 
 // With objects:
@@ -37,3 +55,9 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentWithIds1 = students.map(i => [i.name, i.id]);
+console.log(studentWithIds1)
+
+const studentWithIds = students.map(i => ({name: i.name, id: i.id}));
+console.log(studentWithIds)
